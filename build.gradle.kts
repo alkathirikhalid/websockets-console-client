4d42ec2b055ba0d03c4a532765697798aa34a10e
alkathirikhalid <khalid@alkathirikhalid.com>
2023-11-23 20:27:10 +0800

    Added simple console client

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+val ktor_version: String by project
+
 plugins {
     kotlin("jvm") version "1.9.0"
     application
@@ -10,12 +12,20 @@ repositories {
     mavenCentral()
 }
 
-
-
 kotlin {
     jvmToolchain(8)
 }
 
 application {
     mainClass.set("MainKt")
+}
+
+dependencies {
+    implementation("io.ktor:ktor-client-core:$ktor_version")
+    implementation("io.ktor:ktor-client-cio:$ktor_version")
+    implementation("io.ktor:ktor-client-websockets:$ktor_version")
+}
+
+tasks.named<JavaExec>("run") {
+    standardInput = System.`in`
 }
